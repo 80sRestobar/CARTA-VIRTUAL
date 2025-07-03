32cf0f01bd77817b8df71261cabc3d7c3697a54a
80's Restobar <80srestobarperu@gmail.com>
2025-07-03 12:39:20 -0500

    DD

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function mostrarCarta(tipo) {
   document.getElementById("visorCarta").classList.add('visible');
 
   if (tipo === "platos") {
-    document.getElementById('pdfviewer').src = "assets/Carta Cocina - 80's Restobar.pdf";
+    document.getElementById('pdfviewer').src = "assets/carta-cocina.pdf";
   } else if (tipo === "cocteles") {
-    document.getElementById('pdfviewer').src = "assets/Carta de Cócteles - 80's Restobar.pdf";
+    document.getElementById('pdfviewer').src = "assets/carta-cocteles.pdf";
   }
 }
 
