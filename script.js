a40056a55674a4949016b45508565d590fb6f948
80's Restobar <80srestobarperu@gmail.com>
2025-07-04 22:44:16 -0500

    Prueba con "a"

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function mostrarCarta(tipo) {
   document.getElementById("menuInicial").classList.remove('visible');
   document.getElementById("menuInicial").classList.add("novisible");
-  document.getElementById("visorCarta").classList.remove("novisible")
+  document.getElementById("visorCarta").classList.remove("novisible");
+  document.getElementById("visorCarta").classList.add('visible');
+  document.getElementById("visorCarta").classList.remove("novisible");
   document.getElementById("visorCarta").classList.add('visible');
 
   const contenedor = document.getElementById("cartaImagenes");
   contenedor.innerHTML = "";
+  const pdfviewer = document.querySelector("a");
 
   let folder = "";
   let cantidad = 0;
@@ -13,9 +16,12 @@ function mostrarCarta(tipo) {
   if (tipo === "platos") {
     folder = "carpetacocina";
     cantidad = 8;
+    pdfviewer.href = "assets/carta-cocina.pdf";
+
   } else if (tipo === "cocteles") {
     folder = "carpetabarra";
     cantidad = 7;
+    pdfviewer.href = "assets/carta-cocteles.pdf";
   }
 
   for (let i = 1; i <= cantidad; i++) {
@@ -23,7 +29,7 @@ function mostrarCarta(tipo) {
     img.src = `assets/${folder}/${i}.png`;
     contenedor.appendChild(img);
   }
-}
+
 
 
 function back() {
@@ -32,4 +38,5 @@ function back() {
   document.getElementById('menuInicial').classList.remove('novisible');
   document.getElementById('menuInicial').classList.add('visible');
   document.getElementById('pdfviewer').src = "";
+  document.querySelector("a").href = "";
 }
